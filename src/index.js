3386a72db29e7578ba1455c62a7d9447b2764e88
angoulema <eugenia.labokha@yandex.by>
2020-09-28 22:20:09 +0300

    feat: add all solutions

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 
 exports.min = function min (array) {
-  if (array.length !== 0) {
+  if (Array.isArray(array) && array.length) { // проверка, что это массив и в нем что-то есть
     let minimum = Math.min.apply(null, array);
     return minimum;
   } else return 0;  
-}
+};
 
 exports.max = function max (array) {
-  let maximum = Math.max.apply(null, array);
-  return maximum;
-}
+  if (Array.isArray(array) && array.length) {
+    let maximum = Math.max.apply(null, array);
+    return maximum;
+  } else return 0;
+};
 
 exports.avg = function avg (array) {
-  let average = 0;
-  let sum = 0;
-  for (i = 0; i < array.length; ++i) {
-    if (typeof array[i] === 'number') {
-      sum = sum + array[i];
-    };    
-  };
-  average = sum / array.length;
-  return average;
-}
+  if (Array.isArray(array) && array.length) {
+    let average = 0;
+    let sum = 0;
+    for (i = 0; i < array.length; ++i) {
+      if (typeof array[i] === 'number') {
+        sum = sum + array[i];
+      };    
+    };
+    average = sum / array.length;
+    return average;
+  } else return 0;
+};
